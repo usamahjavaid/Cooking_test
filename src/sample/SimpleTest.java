ba0840685e17a956e0eecaf11452757edfbec211
Usamah Javaid <usamahjavaid@hotmail.com>
2020-12-22 12:08:11 +0100

    Ingredienser og opskrifter oprettet

--- src/sample/SimpleTest.java
+++ src/sample/SimpleTest.java
@@ -2,9 +2,34 @@ package sample;
 
 public class SimpleTest {
 
+
     public static void main(String[] args) {
 
-        System.out.println("Velkommen til min opskrifts-applikation-dummy");
+
+
+        System.out.println("Velkommen til min opskrifts-applikation-dummy\n" );
+
+        Opskrift glasurOpskrift = new Opskrift();
+        System.out.println(glasurOpskrift.navn);
+        Ingrediens flormelis = new Ingrediens();
+        System.out.println(flormelis.flormelis);
+        Ingrediens vand = new Ingrediens();
+        System.out.println(vand.vand);
+
+        Opskrift teOpskrift = new Opskrift();
+        System.out.println(teOpskrift.navn2);
+        Ingrediens tebrev = new Ingrediens();
+        System.out.println(tebrev.tebrev);
+        Ingrediens vand2 = new Ingrediens();
+        System.out.println(vand2.vand2);
+        Ingrediens citron = new Ingrediens();
+        System.out.println(citron.citron);
+
+
+
+
+
+
     }
 
 
