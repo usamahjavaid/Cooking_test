7064da52ed560511dc6284537f2743dd0fcad436
Usamah Javaid <usamahjavaid@hotmail.com>
2020-12-22 09:42:28 +0100

    SimpleTest oprettet.

---
+++ src/sample/SimpleTest.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class SimpleTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Velkommen til min opskrifts-applikation-dummy");
+    }
+
+
+    public void blankLine(){
+
+        System.out.println();
+        System.out.println();
+    }
+}
